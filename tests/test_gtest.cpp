4794c7b076a3e20bcff92916ed1f523515aff9a7
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-04-25 02:14:08 -0400

    Switched to CMake because I couldn't get googletest to work with the Makefile :(

---
+++ tests/test_gtest.cpp
@@ -0,0 +1,12 @@
+#include "../include/helper.h"
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+        // Expect two strings not to be equal.
+        EXPECT_STRNE("hello", "world");
+        // Expect equality.
+        EXPECT_EQ(7 * 6, 42);
+        // test include
+        EXPECT_EQ(get_pos(0,0), 0);
+}
