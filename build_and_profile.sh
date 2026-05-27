7f2932273a2f673415e27762155d6a2ed31ff22a
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-05-27 19:39:02 -0400

    Added script to auto profile code with callgrind

---
+++ build_and_profile.sh
@@ -0,0 +1,5 @@
+cd ./bin
+cmake ./../
+cmake --build . --target scrabble_debug
+valgrind --tool=callgrind ./scrabble_debug ../wordlists/NWL2023-modified.txt ../wordlists/trie.txt ../wordlists/extensions.txt ../games
+
